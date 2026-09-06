fefc5f74d977c214fbf83bde5eaa3d7a21356474
Prabhat Baduwal <baduwalprabhat14@gmail.com>
2026-09-06 23:42:05 +0545

    chore: setting up folder structure

---
+++ app/land/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Land Page</h1>
+    </div>
+  );
+};
+
+export default page;
